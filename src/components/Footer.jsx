62ba7370359c5001366f4158af635f407e7097a6
Kent Leo L. Makibulan <leomakibulan@gmail.com>
2023-09-17 19:19:22 +0800

    add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <>
+      <br />
+      <div className="flex justify-between text-sm my-5">
+        <p>&copy; 2023 Kent Leo L. Makibulan</p>
+        <p>💻 with 💝</p>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
