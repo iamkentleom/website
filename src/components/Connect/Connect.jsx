011bb2e7557a882f1cedbb1e5d8cf1c24535b4bf
Kent Leo L. Makibulan <leomakibulan@gmail.com>
2024-02-03 19:08:42 +0800

    update connect section text and btn redirect

--- src/components/Connect/Connect.jsx
+++ src/components/Connect/Connect.jsx
@@ -1,3 +1,5 @@
+import { TypeAnimation } from "react-type-animation";
+
 const Connect = () => {
   return (
     <section
@@ -6,14 +8,31 @@ const Connect = () => {
       aria-label="Connect with Me"
     >
       <div>
-        <p>Let's get some work done and</p>
+        <p>Let's work together and</p>
         <p>
-          turn those <strong>ideas</strong> into
+          turn your <strong>ideas</strong> into{" "}
+          <TypeAnimation
+            sequence={[
+              "functional applications",
+              3000,
+              "seamless experiences",
+              3000,
+              "innovative solutions",
+              3000,
+              "digital masterpieces",
+              3000,
+            ]}
+            preRenderFirstString={true}
+            wrapper="strong"
+            cursor={false}
+            repeat={Infinity}
+          />
+          .
         </p>
       </div>
       <a
-        href="#"
-        className="px-5 py-3 rounded-lg bg-gray-950 hover:bg-gray-800 text-white"
+        href="mailto:leomakibulan@gmail.com?subject=Work%20Inquiry"
+        className="px-5 py-3 rounded-lg bg-gray-950 hover:bg-gray-800 text-white min-w-fit"
       >
         Hire Me!
       </a>
