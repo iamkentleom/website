50bfcf24ab80e83dbf5e820235ea6b5b9e5b80f2
Kent Leo L. Makibulan <leomakibulan@gmail.com>
2024-02-04 22:31:40 +0800

    add deployment script

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# handle flags
+while getopts m: flag
+do
+    case "${flag}" in
+        m) message=${OPTARG};;
+    esac
+done
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist/
+
+# deploy into iamkentleom.github.io
+git init
+git branch -m master
+git add .
+git commit -S -m "$message"
+git push --set-upstream -f git@github.com:iamkentleom/iamkentleom.github.io.git master
+
+cd -
+
+echo 'Done.'
